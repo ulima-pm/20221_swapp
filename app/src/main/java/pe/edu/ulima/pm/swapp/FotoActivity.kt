cb569566f4a2d980873a6340dad5ba26c5aa5f37
Hernan <hernan@jsatch.com>
2022-06-30 15:52:47 -0500

    Implementacion de foto realizada (falta girarla)

--- app/src/main/java/pe/edu/ulima/pm/swapp/FotoActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/swapp/FotoActivity.kt
@@ -2,15 +2,22 @@ package pe.edu.ulima.pm.swapp
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FotoActivity : AppCompatActivity() {
     private lateinit var iviFoto : ImageView
+    private var mFotoPath : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,23 +32,43 @@ class FotoActivity : AppCompatActivity() {
     }
 
     private fun tomarFoto() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        Log.i("FotoActivity" , "CP1")
-        startActivityForResult(intent, 100)
-        /*intent.resolveActivity(packageManager)?.also {
-            Log.i("FotoActivity" , "CP2")
-
-        }*/
-
-
+        val imageFile = crearArchivoImagen()
+        if (imageFile != null) {
+            val fotoURI = FileProvider.getUriForFile(
+                this,
+                "pe.edu.ulima.pm.swapp.fileprovider",
+                imageFile
+            )
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fotoURI)
+            startActivityForResult(intent, 100)
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 100 && resultCode == RESULT_OK) {
             // Vamos a recibir la foto y pintarla en el imageview
-            val bitmap : Bitmap = data!!.extras!!.get("data") as Bitmap
+            /*val bitmap : Bitmap = data!!.extras!!.get("data") as Bitmap
+            iviFoto.setImageBitmap(bitmap)*/
+
+            val bitmap : Bitmap = BitmapFactory.decodeFile(mFotoPath!!)
             iviFoto.setImageBitmap(bitmap)
         }
+    }
+
+    private fun crearArchivoImagen() : File? {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss")
+            .format(Date())
+        val nombreArchivo = "SWAPP_${timestamp}"
+        val folderAlmacenamiento = getExternalFilesDir(
+            Environment.DIRECTORY_PICTURES)
+        val imageFile = File.createTempFile(
+            nombreArchivo,
+            ".jpg",
+            folderAlmacenamiento
+        )
+        mFotoPath = imageFile.absolutePath
+        return imageFile
     }
 }
